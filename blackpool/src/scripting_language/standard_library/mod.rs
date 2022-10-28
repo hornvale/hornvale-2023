6d150bb048d5873b7463e07eceb9a8127ee145e1
Nathan Douglas <github@darkdell.net>
2022-10-28 15:27:03 -0400

    Comment.

--- blackpool/src/scripting_language/standard_library/mod.rs
+++ blackpool/src/scripting_language/standard_library/mod.rs
@@ -2,6 +2,7 @@ use crate::scripting_language::native_function::error::Error;
 use crate::scripting_language::value::Value;
 use crate::scripting_language::virtual_machine::VirtualMachine;
 
+/// Get the time since the program started.
 #[named]
 pub fn uptime(vm: &VirtualMachine, args: &[Value]) -> Result<Value, Error> {
   trace_enter!();
